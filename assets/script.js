a4fd03ab19ca2cd77492aca1efaac711029d6be2
Daniel McCormick <dcaseymccormick@gmail.com>
2022-08-25 07:30:15 -0500

    app now cycles throgh questions and multiple choice answers w correct and incorrect

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-const question = document.gitElementById("question");
+const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
-let quesionCounter = 0;
+let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
@@ -44,36 +44,53 @@ let questions = [
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESIONS = 3;
+const MAX_QUESIONS = 4;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [ ... questions];
-    console.log(availableQuestions);
     getNewQuestion();
 };
 
 getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESIONS) {
+    //go to the end page
+        return window.location.assign("/end.html");
+    }
+    //get questions
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
-
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
+    // get answer choices
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
         choice.innerText = currentQuestion["choice" + number];
     });
-
+    // splices out asked questions
     availableQuestions.splice(questionIndex, 1);
-
     acceptingAnswers = true;
 
 };
-
+    
 choices.forEach(choice => {
     choice.addEventListener("click", e => {
-        console.log(e.target);
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        const classToApply =
+         selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+         selectedChoice.parentElement.classList.add(classToApply);
+
+         setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();   
+        }, 1000);
     });
 });
 
